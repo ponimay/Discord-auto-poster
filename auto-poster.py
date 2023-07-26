f17fd1b8864d8bdbf9da11cc08644e1b5a1e3939
ponimay <kirpichnikov.nik2019@yandex.ru>
2023-07-26 20:19:57 +0300

    v1.0

--- auto-poster.py
+++ auto-poster.py
@@ -1,29 +1,70 @@
+import tkinter as tk
 import requests
 import time
+import threading
 
-payload = {
-    'content': "message"
-}
+def send_message():
+    channel_id = channel_entry.get()
+    user_header = header_entry.get()
+    cooldown_time = int(cooldown_entry.get())
 
-header = {
-    'Authorization': 'token discord'
-}
+    payload = {
+        'content': "s"
+    }
 
-cooldown_time = 5  # Set the cooldown time in seconds
+    header = {
+        'Authorization': user_header
+    }
 
-while True:
-    try:
-        # Make a request
-        r = requests.post("https://discord.com/api/v9/channels/id_channel/messages", data=payload, headers=header)
+    while not stop_flag.is_set():
+        try:
+            r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=header)
 
-        # Check the response
-        if r.status_code == 200:
-            print("Message sent successfully.")
-        else:
-            print(f"Failed to send the message. Status code: {r.status_code}")
+            if r.status_code == 200:
+                print("Message sent successfully.")
+            else:
+                print(f"Failed to send the message. Status code: {r.status_code}")
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error occurred: {e}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error occurred: {e}")
 
-    # Add a cooldown to avoid making requests too frequently
-    time.sleep(cooldown_time)
+        time.sleep(cooldown_time)
+
+def start_sending():
+    global stop_flag
+    stop_flag.clear()
+    send_thread = threading.Thread(target=send_message)
+    send_thread.start()
+
+def stop_sending():
+    global stop_flag
+    stop_flag.set()
+
+# Create the main window
+window = tk.Tk()
+window.title("Discord Message Sender by n1katio")
+
+# Create and position the widgets
+tk.Label(window, text="Channel ID:").grid(row=0, column=0, padx=5, pady=5)
+channel_entry = tk.Entry(window)
+channel_entry.grid(row=0, column=1, padx=5, pady=5)
+
+tk.Label(window, text="Token:").grid(row=1, column=0, padx=5, pady=5)
+header_entry = tk.Entry(window)
+header_entry.grid(row=1, column=1, padx=5, pady=5)
+
+tk.Label(window, text="Cooldown (seconds):").grid(row=2, column=0, padx=5, pady=5)
+cooldown_entry = tk.Entry(window)
+cooldown_entry.grid(row=2, column=1, padx=5, pady=5)
+
+start_button = tk.Button(window, text="Start", command=start_sending)
+start_button.grid(row=3, column=0, padx=5, pady=5)
+
+stop_button = tk.Button(window, text="Stop", command=stop_sending)
+stop_button.grid(row=3, column=1, padx=5, pady=5)
+
+# Create a flag to stop the sending process
+stop_flag = threading.Event()
+
+# Start the GUI event loop
+window.mainloop()
